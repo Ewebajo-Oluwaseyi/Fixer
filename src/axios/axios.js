b29b667eb1ca52fca983545ec6c5674286043f1a
unknown <seyi@example.com>
2022-01-02 13:53:09 -1100

    hide env

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const KEY = process.env.REACT_APP_API_KEY;
-//export a base url 
+
 export default axios.create({
     baseURL: "https://data.fixer.io/api/",
     params: {
